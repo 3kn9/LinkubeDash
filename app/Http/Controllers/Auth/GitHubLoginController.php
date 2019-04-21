af14dd54fa65a3b39b7525c4ca8e999b3fe6e5eb
cawloft <caw@cawloft.com>
2019-04-21 15:34:45 +0800

    Request for private email address if there isn't a public email

--- app/Http/Controllers/Auth/GitHubLoginController.php
+++ app/Http/Controllers/Auth/GitHubLoginController.php
@@ -33,8 +33,8 @@ class GitHubLoginController extends Controller
             'code' => $code
         ]), $result);
 
-        if(!isset($result['access_token']) || $result['scope'] != 'user:email'){
-            return $this->connectionFailedRedirect();
+        if(!isset($result['access_token'])){
+            return $this->failedRedirect('auth.github_failed');
         }
 
         return $this->processGitHubLogin($result['access_token']);
@@ -47,21 +47,34 @@ class GitHubLoginController extends Controller
             "Authorization: token " . $token
         ]), true);
 
-        if(!isset($result['email'])){
-            return $this->connectionFailedRedirect();
-        }
-
         $users = User::all();
         if($user = $users->where('github_id', $result['id'])->first()){
             Auth::login($user);
             return redirect(route('dashboard'));
         } else {
+
+            if(!isset($result['email'])){
+
+                // If the GitHub user hasn't set any public email address
+                // Send a request to GitHub again to check for private email addresses
+
+                if(!($email = $this->getPrimaryEmail(json_decode(Utils::sendcURLRequest('https://api.github.com/user/emails', [
+                    "User-Agent: Linkube App",
+                    "Authorization: token " . $token
+                ]), true)))){
+                    return $this->failedRedirect('auth.github_invalid_email');
+                }
+
+            } else {
+                $email = $result['email'];
+            }
+
             if($users->where('email', $result['email'])->first()){
-                return $this->emailAlreadyExistRedirect();
+                return $this->failedRedirect('auth.github_email_already_exist');
             } else {
                 User::create([
                     'name' => $result['name'] ?? $result['login'],
-                    'email' => $result['email'],
+                    'email' => $email,
                     'github_id' => $result['id']
                 ]);
                 return redirect(route('dashboard'));
@@ -70,11 +83,16 @@ class GitHubLoginController extends Controller
 
     }
 
-    protected function connectionFailedRedirect(){
-        return redirect(route('login'))->withErrors(['notification' => trans('auth.github_failed')]);
+    protected function failedRedirect($message){
+        return redirect(route('login'))->withErrors(['notification' => trans($message)]);
     }
 
-    protected function emailAlreadyExistRedirect(){
-        return redirect(route('login'))->withErrors(['notification' => trans('auth.github_email_already_exist')]);
+    protected function getPrimaryEmail(array $emails){
+        foreach($emails as $data){
+            if($data['primary'] && $data['verified']){
+                return $data['email'];
+            }
+        }
+        return false;
     }
 }
